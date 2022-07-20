c1aa43e2ae28f4237dcdd0749bdcf3a9dd4f8bf0
edlavack <108073221+edlavack@users.noreply.github.com>
2022-07-20 08:45:06 -0400

    Brian Help

--- js/site.js
+++ js/site.js
@@ -1,14 +1,34 @@
 function getValue(){
-    alert("Hello World")
+    let message = document.getElementById("message").value;
+    let revMessage = reverse(message);
+    displayReversed(revMessage, message);
 }
-function getValue2(){
-    let msg= '';
-    msg=document.getElementById("message").value;
 
-    Swal.fire(
-       { backdrop: false,
-        title: 'App Name',
-        text: msg,
-       }
-    );
+function reverse(message)
+{
+let reversedMessage = "";
+for (let index = message.length-1; index >= 0; index--) {
+   
+    reversedMessage += message[index];
+    
+}
+
+return reversedMessage;
+}
+
+
+function displayReversed(revMessage, message){
+let results = document.getElementById("results");
+results.classList.add("revMessage");
+let displayMessage = "";
+revMessage = revMessage.replace(" ","");
+message = message.replace(" ","");
+
+if (revMessage.toLowerCase() == message.toLowerCase()) {
+    displayMessage = "Good One!";
+} else{
+    displayMessage = "Ehhhh I don't think so";
+}
+
+results.innerHTML = displayMessage;
 }
